521714c8d0dfd444b2f7e78be4f03d17eaca2e09
Aryan Modi <146563774+aryxnmodi@users.noreply.github.com>
2024-02-01 13:16:09 +0530

    Add files via upload

---
+++ Aryan/Module 3/2.php
@@ -0,0 +1,38 @@
+<html lang="en">
+<head>
+    <title>check year is leap year or not</title>
+</head>
+<body>
+    <form>
+       <label> Enter the Year </label> <input type="number" name="LeapYear">
+    <input type="submit" value="submit" name="submitForm"></br></br>
+    </form>
+</body>
+</html>
+<?php
+if(isset($_GET["submitForm"]))
+{
+    $year=$_GET['LeapYear'];
+    if(($year%4)==0)
+    {
+        if(($year%100)==0)
+        {
+            if(($year%400)==0)
+            {
+                echo "$year is a leap year";
+            }
+            else{
+                echo "$year is not a leap year";
+            }
+        }
+        else{
+            echo "$year is a leap year";
+        }
+    }
+    else{
+        echo "$year is not a leap year";
+    }
+}
+?>
+</head>
+</html>
